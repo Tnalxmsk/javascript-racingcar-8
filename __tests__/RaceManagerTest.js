c727a186f487a5c91b95c61ef45bf9972457c868
Tnalxmsk <alstnwkd990@naver.com>
2025-10-23 02:39:33 +0900

    test(RaceManagerTest): 레이스 단일 및 공동 우승자를 찾아 반환하는 기능 테스트
    단일 혹은 공동 우승자를 찾아 반환하는 기능 테스트 코드 작성

--- __tests__/RaceManagerTest.js
+++ __tests__/RaceManagerTest.js
@@ -15,4 +15,35 @@ describe('RaceManager', () => {
     expect(raceManager.currentCount).toBe(5);
     expect(raceManager.maxCount).toBe(5);
   });
+
+  test('레이스 단일 우승자 결정 테스트', () => {
+    const raceManager = new RaceManager(5);
+    const player1 = new Player('minsu');
+    const player2 = new Player('javaji');
+    const player3 = new Player('tong');
+    const players = [player1, player2, player3];
+    player1.move();
+    player1.move();
+    player2.move();
+    const winners = raceManager.determineWinners(players);
+    expect(winners.length).toBe(1);
+    expect(winners[0].name).toBe('minsu');
+  });
+
+  test('레이스 공동 우승자 결정 테스트', () => {
+    const raceManager = new RaceManager(5);
+    const player1 = new Player('minsu');
+    const player2 = new Player('javaji');
+    const player3 = new Player('tong');
+    const players = [player1, player2, player3];
+    player1.move();
+    player1.move();
+    player2.move();
+    player2.move();
+    player3.move();
+    const winners = raceManager.determineWinners(players);
+    expect(winners.length).toBe(2);
+    expect(winners[0].name).toBe('minsu');
+    expect(winners[1].name).toBe('javaji');
+  });
 });
