80f9bd419c2aa9b58f672033537e50230b5206c3
Tnalxmsk <alstnwkd990@naver.com>
2025-10-23 01:45:31 +0900

    feat(input): 시도 횟수 입력 기능 구현

--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -1,10 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
-import { READ_NAMES_MESSAGE } from "../const/input.js";
+import { READ_ATTEMPT_COUNT_MESSAGE, READ_NAMES_MESSAGE } from "../const/input.js";
+import { NAME_SEPARATOR } from "../const/rule.js";
 
 export class InputView {
-  readPlayersName() {
-    const input = Console.readLineAsync(READ_NAMES_MESSAGE);
-    const names = input.split(',');
+  async readPlayersName() {
+    const input = await Console.readLineAsync(READ_NAMES_MESSAGE);
+    const names = input.split(NAME_SEPARATOR);
     return names;
+  }
+
+  async readAttemptCount() {
+    const input = await Console.readLineAsync(READ_ATTEMPT_COUNT_MESSAGE);
+    const attemptCount = parseInt(input);
+    return attemptCount;
   }
 }
