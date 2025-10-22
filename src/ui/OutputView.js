6465e32fe509df7ff1007e0893b622e0f6dc0806
Tnalxmsk <alstnwkd990@naver.com>
2025-10-23 02:22:21 +0900

    feat(OutputView): 레이스게임 출력 기능 구현
    레이스 시작 및 라운드 결과 출력 기능 구현

---
+++ src/ui/OutputView.js
@@ -0,0 +1,20 @@
+import { Console } from "@woowacourse/mission-utils";
+import { DISTANCE_FORMAT, RACE_START_MESSAGE } from "../const/output.js";
+
+export class OutputView {
+
+  printRaceStart() {
+    Console.print(RACE_START_MESSAGE);
+  }
+
+  /**
+   * @param {Player[]} players
+   */
+  printRoundResult(players) {
+    players.forEach((player) => {
+      const { name, distanceCount } = player;
+      Console.print(`${name} : ${DISTANCE_FORMAT.repeat(distanceCount)}`);
+    });
+    console.log();
+  }
+}
