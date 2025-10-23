17811c4adf89251c3b5ce965ffadab5895eea006
Tnalxmsk <alstnwkd990@naver.com>
2025-10-23 17:06:08 +0900

    feat(InputView): 입력 값 검증 단계 추가
    이름, 시도 횟수 입력 값에 대한 검증 진행 코드 작성

--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -1,16 +1,21 @@
 import { Console } from "@woowacourse/mission-utils";
 import { READ_ATTEMPT_COUNT_MESSAGE, READ_NAMES_MESSAGE } from "../const/input.js";
 import { NAME_SEPARATOR } from "../const/rule.js";
+import Validator from "../util/Validator.js";
 
 export class InputView {
   async readPlayersName() {
     const input = await Console.readLineAsync(READ_NAMES_MESSAGE);
+    const validator = new Validator();
+    validator.validateNames(input);
     const names = input.split(NAME_SEPARATOR);
     return names;
   }
 
   async readAttemptCount() {
     const input = await Console.readLineAsync(READ_ATTEMPT_COUNT_MESSAGE);
+    const validator = new Validator();
+    validator.validateAttemptCount(input);
     const attemptCount = parseInt(input);
     return attemptCount;
   }
