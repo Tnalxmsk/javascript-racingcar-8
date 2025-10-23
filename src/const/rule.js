4b0e45c70195e694e61943803dd483e73c997c6a
Tnalxmsk <alstnwkd990@naver.com>
2025-10-23 16:52:25 +0900

    feat(rule): 자동차 게임 입력 값 rule과 관련한 상수 추가

--- src/const/rule.js
+++ src/const/rule.js
@@ -2,3 +2,8 @@ export const MIN_RANDOM_NUMBER = 0;
 export const MAX_RANDOM_NUMBER = 9;
 export const MIN_MOVE_NUMBER = 4;
 export const NAME_SEPARATOR = ',';
+
+export const NAME_FORMAT_REGEX = /^[a-zA-Z가-힣]+$/;
+export const NAME_SEPARATOR_REGEX = /[^a-zA-Z가-힣,]/;
+export const MAX_NAMES_COUNT = 10;
+export const MAX_NAME_LENGTH = 5;
