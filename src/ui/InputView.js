54e58e0144c2db9e0986512fa0864c23c0d2af3e
Tnalxmsk <alstnwkd990@naver.com>
2025-10-24 21:48:11 +0900

    refactor(InputView): Validator 변경에 따른 호출 방식 변경

--- src/ui/InputView.js
+++ src/ui/InputView.js
@@ -1,22 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
 import { READ_ATTEMPT_COUNT_MESSAGE, READ_NAMES_MESSAGE } from "../const/input.js";
-import { NAME_SEPARATOR } from "../const/rule.js";
-import Validator from "../util/Validator.js";
+import { Validator } from "../util/Validator.js";
+import { parseNames } from "../util/parse.js";
 
 export class InputView {
   async readPlayersName() {
     const input = await Console.readLineAsync(READ_NAMES_MESSAGE);
-    const validator = new Validator();
-    validator.validateNames(input);
-    const names = input.split(NAME_SEPARATOR);
-    return names;
+    Validator.validateNames(input);
+    return parseNames(input);
   }
 
   async readAttemptCount() {
     const input = await Console.readLineAsync(READ_ATTEMPT_COUNT_MESSAGE);
-    const validator = new Validator();
-    validator.validateAttemptCount(input);
-    const attemptCount = parseInt(input);
-    return attemptCount;
+    Validator.validateAttemptCount(input);
+    return parseInt(input);
   }
 }
